4ff2b06b797d8decbe042c34cf357694bea1cbe3
Tate Rathje <taterathje@gmail.com>
2017-04-21 02:07:26 -0700

    Setup controller, model, and database connection.

---
+++ pubsub/models/badges.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var redis = require('../lib/redis');
+
+exports.save = function (badges, callback) {
+	if (!badges.length) return callback(null, null);
+	var badge = badges.pop();
+	redis.lpush('badges', JSON.stringify(badge), function (err) {
+		if (err) return callback(err, null);
+		exports.save(badges, callback);
+	});
+};
