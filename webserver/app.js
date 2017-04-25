a25a1ab56660551bcfcda79b49e6df9a928a79f9
Tate Rathje <taterathje@gmail.com>
2017-04-25 03:01:34 -0700

    Started setting up subscribe part of pub/sub on webserver.

--- webserver/app.js
+++ webserver/app.js
@@ -5,6 +5,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+var subSocket = require('./lib/subscribe.js');
+
 server.listen(3000, function () {
 	console.log('Server is running on port %d', 3000);
 });
@@ -18,5 +20,20 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
 	res.sendfile('./public/index.html');
 });
+
+io.socket.on('connection', function (socket) {
+	model.get(function (err, data) {
+		if (err) return;
+		data.forEach(function(badge) {
+			socket.emit('badge', badge);
+		});
+	});
+});
+
+subSocket.on('message', function (message) {
+	io.socket.emit('badge', message);
+});
+
+
 
 
