0a1805cd1e5b39c34a4e67a16eb5164302edbca5
Tate Rathje <taterathje@gmail.com>
2017-04-26 01:58:49 -0700

    Add styles, jquery, and finished getting badges from pub/sub and displaying them on the client.

--- webserver/app.js
+++ webserver/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var express = require('express')
+var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 var subSocket = require('./lib/subscribe.js');
+var badges = require('./models/badges');
 
 server.listen(3000, function () {
 	console.log('Server is running on port %d', 3000);
@@ -21,8 +22,8 @@ app.get('/', function (req, res) {
 	res.sendfile('./public/index.html');
 });
 
-io.socket.on('connection', function (socket) {
-	model.get(function (err, data) {
+io.sockets.on('connection', function (socket) {
+	badges.get(function (err, data) {
 		if (err) return;
 		data.forEach(function(badge) {
 			socket.emit('badge', badge);
@@ -31,7 +32,7 @@ io.socket.on('connection', function (socket) {
 });
 
 subSocket.on('message', function (message) {
-	io.socket.emit('badge', message);
+	io.sockets.emit('badge', message);
 });
 
 
--- webserver/models/badges.js
+++ webserver/models/badges.js
@@ -1,1 +1,15 @@
 'use strict';
+
+var request = require('request');
+
+
+/**
+ * Get badges for the pub/sub server
+ * @para {Function} callback
+ */
+exports.get = function (callback) {
+	request('http://localhost:8000/badges', function (err, response, body) {
+		callback(err, JSON.parse(body));
+	});
+};
+
